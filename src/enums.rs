ae378176e3d721dd9c5558474f058bc722b30dda
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2020-12-06 13:25:50 -0500

    Add some display impls

--- src/enums.rs
+++ src/enums.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Display};
+
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Clan {
     SeaWolves,
@@ -17,7 +19,47 @@ pub enum Clan {
     Helion,
     TheLost,
 }
-
+impl Display for Clan {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.clan_name())
+    }
+}
+impl Clan {
+    pub const fn race_name(&self) -> &'static str {
+        use Clan::*;
+        match self {
+            SeaWolves | Hellsguard => "Roegadyn",
+            Highlander | Midlander => "Hyur",
+            Wildwood | Duskwight => "Elezen",
+            Sun | Moon => "Miqo'te",
+            Plainsfolk | Dunesfolk => "Lalafell",
+            Xaela | Raen => "Au Ra",
+            Rava | Veena => "Viera",
+            Helion | TheLost => "Hrothgar",
+        }
+    }
+    pub const fn clan_name(&self) -> &'static str {
+        use Clan::*;
+        match self {
+            SeaWolves => "Sea Wolves",
+            Hellsguard => "Hellguard",
+            Highlander => "Highlander",
+            Midlander => "Midlander",
+            Wildwood => "Wildwood",
+            Duskwight => "Duskwight",
+            Sun => "Seeker of the Sun",
+            Moon => "Keeper of the Moon",
+            Plainsfolk => "Plainsfolf",
+            Dunesfolk => "Dunesfolk",
+            Xaela => "Xaela",
+            Raen => "Raen",
+            Rava => "Rava",
+            Veena => "Veena",
+            Helion => "Helion",
+            TheLost => "The Lost",
+        }
+    }
+}
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Job {
@@ -69,5 +111,42 @@ impl Job {
     pub const fn caster(&self) -> bool {
         use Job::*;
         matches!(self, THM | BLM | ACN | SMN | RDM | BLU)
+    }
+    pub const fn job_name(&self) -> &'static str {
+        use Job::*;
+        match self {
+            GLA => "Gladiator",
+            PGL => "Pugilist",
+            MRD => "Marauder",
+            LNC => "Lancer",
+            ARC => "Archer",
+            CNJ => "Conjurer",
+            THM => "Thaumaturge",
+            PLD => "Paladin",
+            MNK => "Monk",
+            WAR => "Warrior",
+            DRG => "Dragoon",
+            BRD => "Bard",
+            WHM => "White Mage",
+            BLM => "Black Mage",
+            ACN => "Arcanist",
+            SMN => "Summoner",
+            SCH => "Scholar",
+            ROG => "Rogue",
+            NIN => "Ninja",
+            MCH => "Machinist",
+            DRK => "Dark Knight",
+            AST => "Astrologian",
+            SAM => "Samurai",
+            RDM => "Red Mage",
+            BLU => "Blue Mage",
+            GNB => "Gunbreaker",
+            DNC => "Dancer",
+        }
+    }
+}
+impl Display for Job {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.job_name())
     }
 }
